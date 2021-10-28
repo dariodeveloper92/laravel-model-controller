6c2b48e4eb605a6c946b5c6eb57efdc0434ef755
Dario <dario.defrancisci92@gmail.com>
2021-10-28 17:18:20 +0200

    caricato scaffolding

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+
+{{-- @section('content')
+    <h1>Sono il contenuto della home</h1>
+    <h2>{{ $name }}</h2>
+@endsection --}}
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>Sono l'Header</h1>
+</header>
